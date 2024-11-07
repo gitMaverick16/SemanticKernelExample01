0de64b9575d18c4ba48972962c31ec38220a6e30
Sergio Rondon <sergio19980816@gmail.com>
2024-11-06 21:59:01 -0500

    Agregar archivos de proyecto.

---
+++ SemanticKernelExample01/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SemanticKernelExample01.Models;
+
+namespace SemanticKernelExample01.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Productos { get; set; }
+    }
+}
---
+++ SemanticKernelExample01/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SemanticKernelExample01.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Precio { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        public string Categoria { get; set; }
+    }
+}
